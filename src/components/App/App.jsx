16e20e0d87dd5ab5486a10c1134312c318b1aa52
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-04-19 07:29:25 +0300

    finish update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,12 +22,8 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
-  // const [showBtn, setShowBtn] = useState(false);
-  // const [totalPages, setTotalPages] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
-// useEffect(() => {
-//   setShowBtn(totalPages && totalPages !== page);
-// }, [totalPages, page]);
 
 
 
@@ -35,7 +31,8 @@ export default function App() {
   setQuery(query);
     setPage(1);
     setImages([]);
-  console.log(query);
+    setTotalPages;
+  
   };
 
   const handleLoadMore = () => {
@@ -56,6 +53,7 @@ export default function App() {
         return [...prevImages, ...data.results]
         } 
       );
+      setTotalPages(data.total_pages);
     } catch (error) {
       setError(true);
     } finally {
@@ -100,117 +98,14 @@ export default function App() {
         />
       )}
 
-{/* {showBtn && images.length > 0 && !isLoading && (
-  <LoadMoreBtn onLoadMore={handleLoadMore} />
-)} */}
-      
-      {images.length > 0 &&  !isLoading && <LoadMoreBtn onLoadMore={handleLoadMore} />}
+   {images.length > 0 && page< totalPages && !isLoading && <LoadMoreBtn onLoadMore={handleLoadMore} />}
  
     </>
-    
-  )
+    )
 }
 
 
 
-
-// import { useState, useEffect } from 'react';
-// import { fetchImages } from '../imageSearch-api';
-// import ImageGallery from '../ImageGallery/ImageGallery';
-// import SearchBar from '../SearchBar/SearchBar';
-// import Loader from "../Loader/Loader";
-// import ErrorMessage from '../ErrorMessage/ErrorMessage';
-// import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
-// import ImageModal from '../ImageModal/ImageModal';
-// import Modal from "react-modal";
-
-// Modal.setAppElement('#root');
-
-// export default function App() {
-//   const [images, setImages] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(false);
-//   const [page, setPage] = useState(1);
-//   const [query, setQuery] = useState("");
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [totalPages, setTotalPages] = useState(0);
-//   // const [showBtn, setShowBtn] = useState(false);
-
-//   const handleSearch = async (query) => {
-//     setQuery(query);
-//     setPage(1);
-//     setImages([]);
-    
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage(page + 1);
-//   };
-
-//   useEffect(() => { 
-//     if (query === "") {
-//       return;
-//     }
-
-//     async function getImages() {
-//       try {
-//         setIsLoading(true);
-//         const data = await fetchImages(query, page);
-//         setImages(prevImages => [...prevImages, ...data.results]);
-//         setTotalPages(data.total_pages);
-//         console.log(data.total_pages);
-//         console.log(data.results);
-//       } catch (error) {
-//         setError(true);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     }
-
-//     getImages(); 
-//   }, [page, query]);
-
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <SearchBar onSearch={handleSearch} />
-//       {error && <ErrorMessage />}
-//       {images.length > 0 && (
-//         <ImageGallery 
-//           images={images} 
-//           isOpen={isModalOpen}
-//           openModal={openModal}
-//           closeModal={closeModal}
-//         />
-//       )}
-//       {isLoading && <Loader />}
-//       {isModalOpen && (
-//         <ImageModal
-//           isOpen={isModalOpen}
-//           onClose={closeModal}
-//           selectedImage={images}  
-//         />
-//       )}
-//       {/* {showBtn && <LoadMoreBtn onLoadMore={handleLoadMore} />} */}
-//       {images.length > 0 && page < totalPages && !isLoading && (
-//         <LoadMoreBtn onLoadMore={handleLoadMore} />
-        
-//       )}
-      
-
-//       {/* {showBtn && images.length > 0 && !isLoading && page < totalPages && (
-//   <LoadMoreBtn onLoadMore={handleLoadMore} />
-// )} */}
-//     </>
-//   );
-// }
 
 
 
